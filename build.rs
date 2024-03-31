74c7dfd28233e5508da45d83568381830a0c9b8d
coinmoles <andrewpark421@gmail.com>
2024-03-31 18:29:35 +0900

    Fixed protoc config error

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("protos/stratsync.proto")?;
+    let mut config = prost_build::Config::new();
+    config.protoc_arg("--experimental_allow_proto3_optional");
+    tonic_build::configure().compile_with_config(config, &["protos/stratsync.proto"], &["proto"])?;
     Ok(())
 }
