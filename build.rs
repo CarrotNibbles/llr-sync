ce413a54ece68e0a1ca028f9fce40411544d5af0
hareen <me@hareen.io>
2024-07-30 14:29:11 +0900

    Build protos natively

--- build.rs
+++ build.rs
@@ -1,6 +1,19 @@
+use protox::prost::Message;
+use std::{env, fs, path::PathBuf};
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut config = prost_build::Config::new();
-    config.protoc_arg("--experimental_allow_proto3_optional");
-    tonic_build::configure().compile_with_config(config, &["protos/stratsync.proto"], &["proto"])?;
+    let file_descriptors = protox::compile(["protos/stratsync.proto"], ["."]).unwrap();
+
+    let file_descriptor_path = PathBuf::from(env::var_os("OUT_DIR").expect("OUT_DIR not set"))
+        .join("file_descriptor_set.bin");
+    fs::write(&file_descriptor_path, file_descriptors.encode_to_vec()).unwrap();
+
+    tonic_build::configure()
+        .build_server(true)
+        .file_descriptor_set_path(&file_descriptor_path)
+        .skip_protoc_run()
+        .compile(&["protoc/stratsync.proto"], &["."])
+        .unwrap();
+
     Ok(())
 }
